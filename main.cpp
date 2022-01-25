d0c00917431675ec27fde4f1b48e030f7d5fbea9
Mellowdv <tym.talik@gmail.com>
2022-01-25 23:40:34 +0100

    Simple currency converter

--- main.cpp
+++ main.cpp
@@ -2,7 +2,20 @@
 
 int main()
 {
-    
+    // Currency conversion Try This
+    double amount;
+    int selection;
+    std::cout << "Please enter an amount: ";
+    std::cin >> amount;
+    std::cout << "Please select a currency: 1. Yen, 2. Euro, 3. Pound" << std::endl;
+    std::cin >> selection;
+
+    if (selection == 1)
+        cout << amount << " of yen is " << amount * 0.0088 << " dollars.";
+    else if (selection == 2)
+        cout << amount << " of euros is " << amount * 1.13 << " dollars.";
+    else if (selection == 3)
+        cout << amount << " of pounds is " << amount * 1.35 << " dollars.";
 
     return 0;
 }
